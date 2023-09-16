fa61e3a986963e0dbc84ad5ae50fe6d95ec9880a
srijena guin <100935944+srijena@users.noreply.github.com>
2023-09-17 00:38:30 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -3,11 +3,14 @@ import Hero from "./Hero";
 import Gallerycarousel from "./Gallerycarousel";
 import Whyhmt from "./Whyhmt";
 import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
 
 export {
     Hero,
     Navbar,
     Gallerycarousel,
     Whyhmt,
-    Testimonials
+    Testimonials,
+    Footer
 }
