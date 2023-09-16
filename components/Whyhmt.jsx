0d0a682ad112132db9d5eb053572b0db0d695f28
Parthiba Mukhopadhyay <meparthiba@gmail.com>
2023-09-16 20:00:30 +0530

    Update Whyhmt.jsx

--- components/Whyhmt.jsx
+++ components/Whyhmt.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+'use client'
+import React from 'react';
+
 
 const Whyhmt = () => {
-  return (
-    <div>
-      <h1>Why hmt wala part</h1>
-    </div>
-  )
-}
 
-export default Whyhmt
+
+  return (
+      <div className="albal">
+              <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+      />
+
+      <div className="why-hmt py-5 pb-md-6 pb-lg-7 bg-gray-100">
+        <section className="container animate mx-auto px-4 items-center">
+          <div className="all-partners text-3xl font-bold text-center text-black mb-5">Why HMT Architects?</div>
+          <div className="icons overflow-x-auto flex flex-nowrap items-center justify-center mx-10">
+            <div className="pr-9">
+              <img src="https://super.homelane.com/RTB/installation_days.svg" className="w-40 h-50" />
+            </div>
+            <div className="pr-9">
+              <img src="https://super.homelane.com/RTB/homes_completed.svg" className="w-40 h-50" />
+            </div>
+            <div className="pr-9">
+              <img src="https://super.homelane.com/RTB/design_experts.svg" className="w-40 h-50" />
+            </div>
+            <div className="pr-9">
+              <img src="https://super.homelane.com/RTB/material_warranty.svg" className="w-40 h-50" />
+            </div>
+          </div>
+        </section>
+      </div>
+      </div>
+  );
+};
+
+export default Whyhmt;
