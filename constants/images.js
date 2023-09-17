55d26af848bd75b67db662410742ae3080b40585
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-09-17 21:29:49 +0530

    gallery carousel update

--- constants/images.js
+++ constants/images.js
@@ -10,18 +10,6 @@ import img9 from '../assets/home_project_gallery/9-min-min.jpg';
 import img10 from '../assets/home_project_gallery/10-min-min.jpg';
 import img11 from '../assets/home_project_gallery/11-min-min.jpg';
 import img12 from '../assets/home_project_gallery/12-min-min.jpg';
-import img1 from '../assets/home_project_gallery/1-min-min.jpg';
-import img2 from '../assets/home_project_gallery/2-min-min.jpg';
-import img3 from '../assets/home_project_gallery/3-min-min.jpg';
-import img4 from '../assets/home_project_gallery/4-min-min.jpg';
-import img5 from '../assets/home_project_gallery/5-min-min.jpg';
-import img6 from '../assets/home_project_gallery/6-min-min.jpg';
-import img7 from '../assets/home_project_gallery/7-min-min.jpg';
-import img8 from '../assets/home_project_gallery/8-min-min.jpg';
-import img9 from '../assets/home_project_gallery/9-min-min.jpg';
-import img10 from '../assets/home_project_gallery/10-min-min.jpg';
-import img11 from '../assets/home_project_gallery/11-min-min.jpg';
-import img12 from '../assets/home_project_gallery/12-min-min.jpg';
 
 export default{
     img1,
