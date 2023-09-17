3d0ce6b4123b56d20ddb0cb608c9855949e92915
Parthiba Mukhopadhyay <meparthiba@gmail.com>
2023-09-17 21:09:38 +0530

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -1,7 +1,27 @@
-import img1 from '../assets/img1.jpeg';
-import img2 from '../assets/abc.png';
+import img1 from '../assets/home_project_gallery/1-min-min.jpg';
+import img2 from '../assets/home_project_gallery/2-min-min.jpg';
+import img3 from '../assets/home_project_gallery/3-min-min.jpg';
+import img4 from '../assets/home_project_gallery/4-min-min.jpg';
+import img5 from '../assets/home_project_gallery/5-min-min.jpg';
+import img6 from '../assets/home_project_gallery/6-min-min.jpg';
+import img7 from '../assets/home_project_gallery/7-min-min.jpg';
+import img8 from '../assets/home_project_gallery/8-min-min.jpg';
+import img9 from '../assets/home_project_gallery/9-min-min.jpg';
+import img10 from '../assets/home_project_gallery/10-min-min.jpg';
+import img11 from '../assets/home_project_gallery/11-min-min.jpg';
+import img12 from '../assets/home_project_gallery/12-min-min.jpg';
 
 export default{
     img1,
-    img2
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12
 }
