5b6cfbbd7e2c311c2964c2525968a692baaeb66d
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-09-17 21:15:10 +0530

    gallery carousel update

--- constants/gallery_img.js
+++ constants/gallery_img.js
@@ -7,32 +7,47 @@ export const data = [
     },
     {
       id: 2,
-      img: images.img1,
+      img: images.img2,
     },
     {
       id: 3,
-      img: images.img1,
+      img: images.img3,
     },
     {
       id: 4,
-      img: images.img1,
+      img: images.img4,
     },
     {
       id: 5,
-      img: images.img1,
+      img: images.img5,
     },
     {
       id: 6,
-      img: images.img1,
+      img: images.img6,
     },
     {
       id: 7,
-      img: images.img1,
+      img: images.img7,
     },
     {
       id: 8,
-      img: images.img1,
+      img: images.img8,
     },
-    
+    {
+      id: 9,
+      img: images.img9,
+    },
+    {
+      id: 10,
+      img: images.img10,
+    },
+    {
+      id: 11,
+      img: images.img11,
+    },
+    {
+      id: 12,
+      img: images.img12,
+    },
   ];
   
