a8502e5770b678d16170ae06b14521724402a7e3
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-18 22:12:43 +0530

    Loading Page and a Not Found Page

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <main className='flex flex-col justify-between align-middle text-center'>
+        <h1 className="text-purple-800 font-bold">Loading</h1>
+        <p>Hopefully not too long :)</p>
+    </main>
+  )
+}
