d5d8fd9ec7979188645f24188eda9bf854bda0ed
Anjali Kedia <24kedia.anjali@gmail.com>
2023-09-19 14:13:57 +0545

    contactupdate

--- app/contactus/page.js
+++ app/contactus/page.js
@@ -1,7 +1,55 @@
+import { Contactus } from "@/components";
+import Iframe from 'react-iframe'
 export default function Contact() {
     return (
-        <main>
-            <h1>Contact Us Page</h1>
-        </main>
+                
+            <div className="flex md:flex-row flex-col ">
+                <div className="xl:w-2/5 lg:w-2/5 bg-white py-16 xl:rounded-bl rounded-tl rounded-tr xl:rounded-tr-none flex basis-1/2 mx-auto">
+                    <div className="xl:w-5/6 xl:px-0 px-8 mx-auto">
+                        <h1 className="xl:text-4xl text-3xl pb-4 text-black font-bold">Drop By Our Office</h1>
+                        <p className="text-lg text-black pb-8 leading-relaxed font-normal lg:pr-4">Visit us and have coffee with us while we discuss the plans for your future project.</p>    
+                        <div className="flex pb-4 items-center">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone-call " width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="white" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <path d="M4 4h5l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v5a1 1 0 0 1 -1 1a16 16 0 0 1 -16 -16a1 1 0 0 1 1 -1" />
+                                    <path d="M15 7a2 2 0 0 1 2 2" />
+                                    <path d="M15 3a6 6 0 0 1 6 6" />
+                                </svg>
+                            </div>
+                            <p className="pl-4 text-black text-base">+91 983 321 321</p>
+                        </div>
+                        <div className="flex items-center">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail mb-6" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="white" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <rect x={3} y={5} width={18} height={14} rx={2} />
+                                    <polyline points="3 7 12 13 21 7" />
+                                </svg>
+                            </div>
+                            <p className="pl-4 text-black text-base mb-6">hmtarchitects106@gmail.com</p>
+                        </div>                    
+                        <div className="flex pb-2 items-center">
+                        <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d472.0311046943877!2d80.21173595260018!3d13.041956172795492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5266e94193970f%3A0x621ebe35d760ea0!2sHMT%20Architects!5e0!3m2!1sen!2sin!4v1666200523162!5m2!1sen!2sin"
+                            width="640px"
+                            height="300px"
+                            id=""
+                            className=""
+                            display="block"
+                            position="relative"/>
+                            
+                        </div>
+                        <div className="flex items-center">
+                           
+                        <a href="/projects">
+                            <p className="text-black pt-2  tracking-wide underline">View Our Projects</p>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+
+            <Contactus/>
+            </div>
+        
     )
 }
--- components/Contactus.jsx
+++ components/Contactus.jsx
@@ -30,67 +30,70 @@ export default function Contactus() {
     }).then(() => setQuery({ name: "", email: "", phone: "", message: "" }));
   };
   return (
-    <div className="flex flex-row">
+    <div className="flex mx-auto">
     
-    <div className="flex basis-1/2">
-        <h1>hi</h1>
-
-    </div>
-    <div className="App">
-      <h1>Get in Touch</h1>
+    <div className="m-8 border-2 p-8 shadow-lg border-white rounded-md mx-auto ">
+      <h1 className="text-center font-semibold text-2xl mt-2">Get In Touch</h1>
       <form className="m-4" onSubmit={formSubmit}>
-        <div>
-          <label>Name</label>
+        <div className="mb-4">
+        <label>Name</label>
           <input
             type="text"
             name="name"
             required
-            placeholder="Name"
-            className="form-control"
+            placeholder="Enter your Name"
+            className="form-control border-2  p-2 w-full"
             value={query.name}
             onChange={handleParam()}
           />
         </div>
-        <div>
+        <div className="mb-4">
           <label>Email</label>
           <input
             type="email"
             name="email"
             required
             placeholder="Email"
-            className="form-control"
+            className="form-control  border-2  p-2 w-full"
             value={query.email}
             onChange={handleParam()}
           />
         </div>
-        <div>
+        <div className="mb-4">
           <label>Phone</label>
           <input
             type="phone"
             name="phone"
             required
             placeholder="Phone number"
-            className="form-control"
+            className="form-control  border-2  p-2 w-full"
             value={query.phone}
             onChange={handleParam()}
           />
         </div>
-        <div>
+        <div className="mb-4">
           <label>Message</label>
           <input
             type="text"
             name="message"
             required
             placeholder="Message"
-            className="form-control"
+            className="form-control   border-2  p-2 w-full"
             value={query.message}
             onChange={handleParam()}
           />
         </div>
-        <button type="submit">Send</button>
+        <button className="bg-[#0e3548] w-full p-2 text-white rounded-md " type="submit">Send</button>
       </form>
     </div>
     
     </div>
   );
 }
+
+
+
+
+
+
+
