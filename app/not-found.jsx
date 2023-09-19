1e52801089ac40ad45f026843baf371a507c7f55
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-19 15:15:52 +0530

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
-
 export default function Notfound() {
   return (
     <main className='text-center'>
         <h1 className='text-3xl text-purple-700 font-bold'>There was a problem</h1>
-        <p className="text-blue-500"> Click here Go Back to <Link href="/">Home</Link></p>
+        <p className="text-blue-500"> <Link href="/"> Click here Go Back to Home</Link></p>
         
     </main>
     
