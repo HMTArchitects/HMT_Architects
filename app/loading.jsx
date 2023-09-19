6d60f6e7c5f4fecc13349ddc92e550ecc87ad6e1
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-19 15:15:05 +0530

    Update loading.jsx

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
 export default function Loading() {
   return (
     <main className='flex flex-col justify-between align-middle text-center'>
-        <h1 className="text-purple-800 font-bold">Loading</h1>
+        <h1 className='text-3xl text-purple-700 font-bold'>Loading</h1>
         <p>Hopefully not too long :)</p>
     </main>
   )
