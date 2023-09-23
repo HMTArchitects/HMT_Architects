ee01f90cb3e8236a926994c340964c0d794f8974
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-23 16:22:09 +0530

    Update Residential.jsx

--- components/Residential.jsx
+++ components/Residential.jsx
@@ -1,26 +1,28 @@
 'use client'
+import Link from "next/link.js";
 import { data } from "../constants/project_residential.js";
 import Image from "next/image";
 export default function Residential() {
   return (
       <div className="flex flex-col">
         <div className="flex flex-col">
-          <div className="pl-8 pt-8">
+          <div className="pl-8">
             <h1 className="text-left font-semibold text-4xl mt-2">
               Residential
             </h1>
           </div>
           <div>
-            <div className="m-2 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-2 pl-6 pr-6">
+            <div className="m-2 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-2">
               {data.map((item) => (
-                <div className="inline-block p-5 w-full h-full " key={item.id}>
-                  <Image
-                    className="cursor-pointer drop-shadow-xl hover:scale-110 ease-in-out duration-300 shadow-lg rounded-xl aspect-video w-full h-auto"
+                <div className="inline-block p-6 " key={item.id}>
+                  <Link href={item.link}>
+                    <Image
+                    className="cursor-pointer hover:scale-110 ease-in-out duration-300 shadow-lg rounded-xl aspect-video w-full h-auto"
                     src={item.img}
                     width={400}
                     height={300}
                     alt="/"
-                  />
+                  /></Link>
                 </div>
               ))}
             </div>
