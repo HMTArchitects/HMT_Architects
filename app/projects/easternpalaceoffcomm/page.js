93603d86ac0a637711d4a026f070d06aeed5e7e3
srijena <srijenaguin@gmail.com>
2023-09-24 19:46:46 +0530

    changes

--- app/projects/easternpalaceoffcomm/page.js
+++ app/projects/easternpalaceoffcomm/page.js
@@ -1,18 +1,18 @@
 'use client'
-import { data } from "../../../constants/epalace_offcomm.js";
+import { data } from "../../../constants/mbit_office.js";
 import { Footer } from "@/components";
 import Image from "next/image";
-export default function epalace_offcomm() {
+export default function mbit_office() {
   return (
     <>
       <div className="flex flex-col">
-        <div className="bg-white mb-5 pt-5 pb-3">
-          <h1 className="text-6xl pt-5 font-semibold text-black text-center ">
+        <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+          <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center">
             Eastern Palace
           </h1>
         </div>
-        <div className="flex flex-row mt-10">
-          <div className="w-1/3 mt-10 ml-20 flex flex-col">
+        <div className="flex md:flex-row flex-col mt-10">
+          <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
             <div>
               <h1 className="text-2xl  font-semibold text-black">Scope</h1>
               <p>Interiors</p>
@@ -25,29 +25,29 @@ export default function epalace_offcomm() {
               <h1 className="text-2xl pt-5 font-semibold text-black">
                 Location
               </h1>
-              <p>Parrys,Chennai</p>
+              <p>Parrys, Chennai</p>
             </div>
             <div>
               <h1 className="text-2xl pt-5 font-semibold text-black">
                 Description
               </h1>
               <p>
-              The interiors for a hotel facility has been done based on the wooden theme with raw texture usage along with marble cladding and detailing.
+                The interiors for a hotel facility has been done based on the wooden theme with raw texture usage along with marble cladding and detailing.
               </p>
             </div>
           </div>
-          <div className="w-2/3 c">
-            <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center gap-y-2 p-6">
+          <div className="md:w-2/3 c">
+            <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center mr-12 ml-8 p-6">
               {data.map((item) => (
                 <div
-                  className="inline-block p-6"
+                  className="inline-block pt-6 pl-6 h-full w-full"
                   key={item.id}
                 >
                   <Image
                     className="cursor-pointer hover:scale-110 ease-in-out duration-300 shadow-lg rounded-xl aspect-video w-full h-auto"
                     src={item.img}
-                    width={400}
-                    height={300}
+                    width={300}
+                    height={200}
                     alt="/"
                   />
                 </div>
@@ -60,3 +60,4 @@ export default function epalace_offcomm() {
     </>
   );
 }
+
--- app/projects/uthifactoryoffcomm/page.js
+++ app/projects/uthifactoryoffcomm/page.js
@@ -1,18 +1,18 @@
 'use client'
-import { data } from "../../../constants/uthfactory_offcomm.js";
+import { data } from "../../../constants/mbit_office.js";
 import { Footer } from "@/components";
 import Image from "next/image";
-export default function uthfactory_offcomm() {
+export default function mbit_office() {
     return (
         <>
             <div className="flex flex-col">
-                <div className="bg-white mb-5 pt-5 pb-3">
-                    <h1 className="text-6xl pt-5 font-semibold text-black text-center ">
+                <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+                    <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center">
                         Uthiramerur Factory
                     </h1>
                 </div>
-                <div className="flex flex-row mt-10">
-                    <div className="w-1/3 mt-10 ml-20 flex flex-col">
+                <div className="flex md:flex-row flex-col mt-10">
+                    <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
                         <div>
                             <h1 className="text-2xl  font-semibold text-black">Scope</h1>
                             <p>Architecture, Interior, Electrical & Plumbing</p>
@@ -36,22 +36,22 @@ export default function uthfactory_offcomm() {
 
                                 They are involved in the manufacturing of windmill and their components.
 
-                                The design evolved from the concept of windmills giving them a unique identity
+                                The design evolved from the concept of windmills giving them a unique identity.
                             </p>
                         </div>
                     </div>
-                    <div className="w-2/3 c">
-                        <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center gap-y-2 p-6">
+                    <div className="md:w-2/3 c">
+                        <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center mr-12 ml-8 p-6">
                             {data.map((item) => (
                                 <div
-                                    className="inline-block p-6"
+                                    className="inline-block pt-6 pl-6 h-full w-full"
                                     key={item.id}
                                 >
                                     <Image
                                         className="cursor-pointer hover:scale-110 ease-in-out duration-300 shadow-lg rounded-xl aspect-video w-full h-auto"
                                         src={item.img}
-                                        width={400}
-                                        height={300}
+                                        width={300}
+                                        height={200}
                                         alt="/"
                                     />
                                 </div>
@@ -64,3 +64,4 @@ export default function uthfactory_offcomm() {
         </>
     );
 }
+
