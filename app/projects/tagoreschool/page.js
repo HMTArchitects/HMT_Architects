4b17c59d614769a93693b8e4c3ab9e1e162a8fa4
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-09-24 20:13:20 +0530

    updates

--- app/projects/tagoreschool/page.js
+++ app/projects/tagoreschool/page.js
@@ -6,13 +6,13 @@ export default function tagoreschool() {
   return (
     <>
       <div className="flex flex-col">
-        <div className="bg-white mb-5 pt-5 pb-3">
-          <h1 className="text-6xl pt-5 font-semibold text-black text-center ">
+        <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+          <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center">
             Tagore School
           </h1>
         </div>
-        <div className="flex flex-row mt-10">
-          <div className="w-1/3 mt-10 ml-20 flex flex-col">
+        <div className="flex md:flex-row flex-col mt-10">
+          <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
             <div>
               <h1 className="text-2xl  font-semibold text-black">Scope</h1>
               <p>Architecture, Structural, Electrical & Plumbing.</p>
@@ -39,15 +39,18 @@ export default function tagoreschool() {
               </p>
             </div>
           </div>
-          <div className="w-2/3 c">
-            <div className="m-2 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 justify-items-center gap-y-2 p-6">
+          <div className="md:w-2/3 c">
+            <div className="m-2 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 justify-items-center mr-12 ml-8 p-6">
               {data.map((item) => (
-                <div className="inline-block p-6 " key={item.id}>
+                <div
+                  className="inline-block pt-6 pl-6 h-full w-full"
+                  key={item.id}
+                >
                   <Image
                     className="cursor-pointer hover:scale-110 ease-in-out duration-300 shadow-lg rounded-xl aspect-video w-full h-auto"
                     src={item.img}
-                    width={400}
-                    height={300}
+                    width={300}
+                    height={200}
                     alt="/"
                   />
                 </div>
