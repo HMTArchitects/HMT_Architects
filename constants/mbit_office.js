9fdd639d87966e57d554ebefd7858ae9e1e86e9c
AnjaliKedia <91176852+Anjali-Kedia@users.noreply.github.com>
2023-09-24 10:38:19 +0545

    Add files via upload

---
+++ constants/mbit_office.js
@@ -0,0 +1,49 @@
+import img1 from 'assets/Office&Commercial/4.MBit/1-min.jpg'
+import img2 from 'assets/Office&Commercial/4.MBit/2-min.jpg'
+import img3 from 'assets/Office&Commercial/4.MBit/3-min.jpg'
+import img4 from 'assets/Office&Commercial/4.MBit/4-min.jpg'
+import img5 from 'assets/Office&Commercial/4.MBit/5-min.jpg'
+import img6 from 'assets/Office&Commercial/4.MBit/6-min.jpg'
+import img7 from 'assets/Office&Commercial/4.MBit/7-min.jpg'
+import img8 from 'assets/Office&Commercial/4.MBit/8-min.jpg'
+import img9 from 'assets/Office&Commercial/4.MBit/9-min.jpg'
+
+export const data = [
+    {
+      id: 1,
+      img:img1,
+    },
+    {
+      id: 2,
+      img:img2,
+    },
+    {
+      id: 3,
+      img:img3,
+    },
+    {
+      id: 4,
+      img:img4,
+    },
+    {
+      id: 5,
+      img:img5,
+    },
+    {
+      id: 6,
+      img:img6,
+    },
+    {
+      id: 7,
+      img:img7,
+    },
+    {
+      id: 8,
+      img:img8,
+    },
+    {
+      id: 9,
+      img:img9,
+    },
+  ];
+  
