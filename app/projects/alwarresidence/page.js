4ab7ff35faa6b66c40deecd9f70879ef494666d6
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-24 20:18:46 +0530

    Update page.js

--- app/projects/alwarresidence/page.js
+++ app/projects/alwarresidence/page.js
@@ -6,13 +6,13 @@ export default function alwar_resi() {
   return (
     <>
       <div className="flex flex-col">
-        <div className=" bg-white mb-5 pt-5 pb-3 ">
-          <h1 className="text-6xl pt-5 font-semibold text-black text-center">
+        <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+          <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center underline underline-offset-4">
             Dr. Alwar's Residence
           </h1>
         </div>
-        <div className="flex flex-row mt-10">
-          <div className="w-1/3 mt-10 ml-20 flex flex-col">
+        <div className="flex md:flex-row flex-col mt-10">
+          <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
             <div>
               <h1 className="text-2xl  font-semibold text-black">Scope</h1>
               <p>Interiors</p>
@@ -40,7 +40,7 @@ export default function alwar_resi() {
               </p>
             </div>
           </div>
-          <div className="w-2/3 c">
+          <div className="md:w-2/3 c">
             <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center mr-12 ml-8 p-6">
               {data.map((item) => (
                 <div
