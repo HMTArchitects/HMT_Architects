aee0089aa262d4651bea4711e639657783b54b92
AnjaliKedia <91176852+Anjali-Kedia@users.noreply.github.com>
2023-09-24 10:32:11 +0545

    Add files via upload

---
+++ app/projects/adyarinterior/page.js
@@ -0,0 +1,63 @@
+'use client'
+import { data } from "../../../constants/adyar_interior_office.js";
+import { Footer } from "@/components";
+import Image from "next/image";
+export default function adyar_interior_office() {
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+          <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center underline underline-offset-4">
+            Adyar Interior
+          </h1>
+        </div>
+        <div className="flex md:flex-row flex-col mt-10">
+          <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
+            <div>
+              <h1 className="text-2xl  font-semibold text-black">Scope</h1>
+              <p>Interiors, Electrical & Plumbing</p>
+            </div>
+            <div>
+              <h1 className="text-2xl pt-5 font-semibold text-black">Area</h1>
+              <p>900sft</p>
+            </div>
+            <div>
+              <h1 className="text-2xl pt-5 font-semibold text-black">
+                Location
+              </h1>
+              <p>Chennai</p>
+            </div>
+            <div>
+              <h1 className="text-2xl pt-5 font-semibold text-black">
+                Description
+              </h1>
+              <p>
+              Interiors for a boutique in a dynamic arrangement.
+              </p>
+            </div>
+          </div>
+          <div className="md:w-2/3 c">
+            <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center mr-12 ml-8 p-6">
+              {data.map((item) => (
+                <div
+                  className="inline-block pt-6 pl-6 h-full w-full"
+                  key={item.id}
+                >
+                  <Image
+                    className="cursor-pointer hover:scale-110 ease-in-out duration-300 shadow-lg rounded-xl aspect-video w-full h-auto"
+                    src={item.img}
+                    width={300}
+                    height={200}
+                    alt="/"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
---
+++ app/projects/venkatMahal_office.js
@@ -0,0 +1,9 @@
+import img1 from 'assets/Office&Commercial/7.VenkatMahal/1-min.jpg'
+
+export const data = [
+    {
+      id: 1,
+      img:img1,
+      
+    },
+  ];
