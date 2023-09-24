19403224f4fb966b430daec355491157638fa1b5
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-24 09:02:53 +0530

    Update project_residential.js

--- constants/project_residential.js
+++ constants/project_residential.js
@@ -11,25 +11,25 @@ import img9 from 'assets/Residential/Mrs. Sangeetha.jpg'
 export const data = [
     {
       id: 1,
-      img:img1,
+      img:img8,
       link: '../projects/senthilresidence',
       name: "Mr. Senthil's"
     },
     {
       id: 2,
-      img:img2,
+      img:img4,
       link: '../projects/hariresidence',
       name: "Mr. Hari's"
     },
     {
       id: 3,
-      img:img3,
+      img:img7,
       link: '../projects/saravananresidence',
       name: "Mr. Saravanan's"
     },
     {
       id: 4,
-      img:img4,
+      img:img1,
       link: '../projects/alwarresidence',
       name: "Dr. Alwar's"
     },
@@ -41,25 +41,25 @@ export const data = [
     },
     {
       id: 6,
-      img:img6,
+      img:img2,
       link: '../projects/periyasamiresidence',
       name: "Mr. Periyasami's"
     },
     {
       id: 7,
-      img:img7,
+      img:img6,
       link: '../projects/sarathresidence',
       name: "Mr. Sarath's"
     },
     {
       id: 8,
-      img:img8,
+      img:img9,
       link: '../projects/sangeetharesidence',
       name: "Mrs. Sangeetha's"
     },
     {
       id: 9,
-      img:img9,
+      img:img3,
       link: '../projects/ashokresidence',
       name: "Mr. Ashok"
     },
