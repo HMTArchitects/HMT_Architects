944c46a79b38f83477c51f734ad487c0572c2353
Mainak Chattopadhyay <chatterjee.mainak15@gmail.com>
2023-09-24 20:22:15 +0530

    Update page.js

--- app/projects/senthilresidence/page.js
+++ app/projects/senthilresidence/page.js
@@ -6,13 +6,13 @@ export default function senthil_resi() {
   return (
     <>
       <div className="flex flex-col">
-        <div className="bg-white mb-5 pt-5 pb-3">
-          <h1 className="text-6xl pt-5 font-semibold text-black text-center">
+        <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+          <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center underline underline-offset-4">
             Mr. Senthil's Residence
           </h1>
         </div>
-        <div className="flex flex-row mt-10">
-          <div className="w-1/3 mt-10 ml-20 flex flex-col">
+        <div className="flex md:flex-row flex-col mt-10">
+          <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
             <div>
               <h1 className="text-2xl  font-semibold text-black">Scope</h1>
               <p>Architecture, Structural, Electrical, Plumbing & Interiors</p>
@@ -32,15 +32,15 @@ export default function senthil_resi() {
                 Description
               </h1>
               <p>
-              This residence is designed for an elite businessman.
+                This residence is designed for an elite businessman.
 
-It has all contemporary details with a minimalistic approach to design.
+                It has all contemporary details with a minimalistic approach to design.
 
-It is a classic example of simplicity and elegance.
+                It is a classic example of simplicity and elegance.
               </p>
             </div>
           </div>
-          <div className="w-2/3 c">
+          <div className="md:w-2/3 c">
             <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center mr-12 ml-8 p-6">
               {data.map((item) => (
                 <div
