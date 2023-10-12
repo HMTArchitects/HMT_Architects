09688eb0c147c78ef02e137da68d9e723f0eb172
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2023-10-12 13:49:57 +0000

    Making the loading and not-found functions as async

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-export default function Loading() {
+export default async function Loading() {
   return (
     <main className='flex flex-col justify-between align-middle text-center'>
         <h1 className='text-3xl text-purple-700 font-bold'>Loading</h1>
