2563ee94d1f84c817dd7379f782bc17aad3783b5
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2023-10-12 13:52:10 +0000

    Added alt for images

--- components/Whyhmt.jsx
+++ components/Whyhmt.jsx
@@ -15,15 +15,15 @@ const Whyhmt = () => {
           <div className="all-partners pb-5 text-4xl font-semibold text-center text-black mb-5">Why HMT Architects?</div>
           <div className="grid grid-cols-3 gap-20 mx-5 align-items-center justify-center">
             <div className="md:shrink-0 mx-auto">
-              <Image src={logo2} className='w-40 h-50 sm:w-40 sm:h-50'/>
+              <Image src={logo2} className='w-40 h-50 sm:w-40 sm:h-50' alt = "Design Experts"/>
               <h2 className='text-lg font-bold text-center  text-black mb-5'>Design Experts</h2>
             </div>
             <div className="md:shrink-0 mx-auto">
-              <Image src={logo3} className='w-40 h-50 sm:w-40 sm:h-50'/>
+              <Image src={logo3} className='w-40 h-50 sm:w-40 sm:h-50' alt = "Happy Customers"/>
               <h2 className='text-lg font-bold text-center text-black mb-5'>Happy Customers</h2>
             </div>
             <div className="md:shrink-0 mx-auto">
-              <Image src={logo4} className='w-40 h-50 sm:w-40 sm:h-50'/>
+              <Image src={logo4} className='w-40 h-50 sm:w-40 sm:h-50' alt = "Security"/>
               <h2 className='text-lg font-bold text-center  text-black mb-5'>Security</h2>
             </div>
           </div>
