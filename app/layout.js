318f381dc2aedbe9f5e96c492ac092c44b824926
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2023-10-12 22:39:44 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     <meta http-equiv="Content-Security-Policy" content="script-src 'unsafe-inline'"></meta>
       <body> 
         <Navbar/>
         <main className={font.className} >
