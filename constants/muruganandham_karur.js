9dd2cf811f56a68e6ed96e1820f941c637637fdd
srijena guin <100935944+srijena@users.noreply.github.com>
2023-11-18 09:19:59 +0000

    added images

---
+++ constants/muruganandham_karur.js
@@ -0,0 +1,14 @@
+import img1 from 'assets/Residential/Muruganandham, Karur/1-min.jpg'
+import img2 from 'assets/Residential/Muruganandham, Karur/2-min.jpg'
+
+export const data = [
+    {
+      id: 1,
+      img:img1,
+    },
+    {
+      id: 2,
+      img:img2,
+    },
+  ];
+  
