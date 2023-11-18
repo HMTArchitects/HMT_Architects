8b7e775e0cb863fd5355fa232caf4d22320971ed
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2023-11-18 08:10:58 +0000

    Added pop up in projects

--- app/projects/anandresidence/page.js
+++ app/projects/anandresidence/page.js
@@ -98,7 +98,7 @@ Wood is used as the main element in the design along with marble.
                 />
                 <div className="pt-3 pb-3">
                   <h1 className="text-xl  text-white text-center">
-                    Meenakshi Vishwa Vidyashram
+                    Anand Residence
                   </h1>
                 </div>
               </div>
