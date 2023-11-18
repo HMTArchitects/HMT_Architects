636abc33320ebc2d8ca94286be865e80ed8afca7
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2023-11-18 09:15:53 +0000

    Making the page responsive for phones

--- app/projects/sarathresidence/page.js
+++ app/projects/sarathresidence/page.js
@@ -20,13 +20,13 @@ export default function sarath_resi() {
   return (
     <>
       <div className="flex flex-col">
-        <div className=" bg-white mb-5 pt-5 pb-3 ">
-          <h1 className="text-6xl pt-5 font-semibold text-black text-center underline underline-offset-4">
+        <div className=" bg-white md:mb-5 pt-5 pb-3 ">
+          <h1 className="text-3xl md:text-6xl pt-5 font-semibold text-black text-center">
           Mr. Sarath's Residence
           </h1>
         </div>
-        <div className="flex flex-row mt-10">
-          <div className="w-1/3 mt-10 ml-20 flex flex-col">
+        <div className="flex md:flex-row flex-col mt-10">
+          <div className="md:w-1/3 mt-10 ml-20 flex flex-col">
             <div>
               <h1 className="text-2xl  font-semibold text-black">Scope</h1>
               <p>Architecture, Structural, Interiors, Electrical & Plumbing</p>
@@ -54,7 +54,7 @@ Sublimed elements have been used in the design of the villa
               </p>
             </div>
           </div>
-          <div className="w-2/3 c">
+          <div className="md:w-2/3 c">
             <div className="m-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-items-center mr-12 ml-8 p-6">
               {data.map((item) => (
                 <div
